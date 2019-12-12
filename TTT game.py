e0202e7d49aa43ce3336aedc370ae1989f2cf484
Ayush19-01 <57313104+Ayush19-01@users.noreply.github.com>
2019-12-12 19:24:06 +0530

    Update TTT game.py

--- TTT game.py
+++ TTT game.py
@@ -13,8 +13,21 @@ def disableButton():
     button7.configure(state=DISABLED)
     button8.configure(state=DISABLED)
     button9.configure(state=DISABLED)
+def enablebuttton(event):
+    if p2.get()=="" or p1.get()=="":
+        tkinter.messagebox.showinfo("Name", "Please enter player names.")
+    else:
+        button1.configure(state=NORMAL)
+        button2.configure(state=NORMAL)
+        button3.configure(state=NORMAL)
+        button4.configure(state=NORMAL)
+        button5.configure(state=NORMAL)
+        button6.configure(state=NORMAL)
+        button7.configure(state=NORMAL)
+        button8.configure(state=NORMAL)
+        button9.configure(state=NORMAL)
 def btnClick(buttons):
-    global bclick, flag, player2_name, player1_name, pb, pa
+    global bclick,flag,p1, p2, pb, pa
     if buttons["text"] == " " and bclick == True:
         buttons["text"] = "X"
         bclick = False
@@ -33,33 +46,35 @@ def btnClick(buttons):
         tkinter.messagebox.showinfo("Tic-Tac-Toe", "Button already Clicked!")
 
 def newgame1(event):
+    global bclick,flag
+    bclick = True
+    flag = 0
     root.destroy()
     GUI()
 def checkForWin():
     if (button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X' or
         button4['text'] == 'X' and button5['text'] == 'X' and button6['text'] == 'X' or
         button7['text'] =='X' and button8['text'] == 'X' and button9['text'] == 'X' or
+        button3['text'] == 'X' and button6['text'] == 'X' and button9['text'] == 'X' or
         button1['text'] == 'X' and button5['text'] == 'X' and button9['text'] == 'X' or
         button3['text'] == 'X' and button5['text'] == 'X' and button7['text'] == 'X' or
-        button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X' or
         button1['text'] == 'X' and button4['text'] == 'X' and button7['text'] == 'X' or
-        button2['text'] == 'X' and button5['text'] == 'X' and button8['text'] == 'X' or
-        button7['text'] == 'X' and button6['text'] == 'X' and button9['text'] == 'X'):
+        button2['text'] == 'X' and button5['text'] == 'X' and button8['text'] == 'X'):
         disableButton()
         tkinter.messagebox.showinfo("Tic-Tac-Toe", pa)
 
     elif(flag == 8):
         tkinter.messagebox.showinfo("Tic-Tac-Toe", "It is a Tie")
+        disableButton()
 
     elif (button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O' or
           button4['text'] == 'O' and button5['text'] == 'O' and button6['text'] == 'O' or
           button7['text'] == 'O' and button8['text'] == 'O' and button9['text'] == 'O' or
           button1['text'] == 'O' and button5['text'] == 'O' and button9['text'] == 'O' or
           button3['text'] == 'O' and button5['text'] == 'O' and button7['text'] == 'O' or
-          button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O' or
+          button3['text'] == 'O' and button6['text'] == 'O' and button9['text'] == 'O' or
           button1['text'] == 'O' and button4['text'] == 'O' and button7['text'] == 'O' or
-          button2['text'] == 'O' and button5['text'] == 'O' and button8['text'] == 'O' or
-          button7['text'] == 'O' and button6['text'] == 'O' and button9['text'] == 'O'):
+          button2['text'] == 'O' and button5['text'] == 'O' and button8['text'] == 'O'):
         disableButton()
         tkinter.messagebox.showinfo("Tic-Tac-Toe", pb)
 def GUI():
@@ -68,6 +83,7 @@ def GUI():
     root= Tk()
     root.title("Tic Tac Toe")
     root.config(bg="#220047")
+    root.resizable(0,0)
     
     pa=StringVar()
     pb=StringVar()
@@ -76,25 +92,27 @@ def GUI():
 
 
     player1_name = Entry(root, textvariable=p1, bd=5)
-    player1_name.grid(row=3, column=4,rowspan=2,columnspan=2)
+    player1_name.grid(row=3, column=4)
     
     player2_name = Entry(root, textvariable=p2, bd=5)
-    player2_name.grid(row=4, column=4,rowspan=2,columnspan=2)
+    player2_name.grid(row=4, column=4)
 
-    nt1=Button(root,text="New Game",font='roboto 30',bg="#CE9141", fg="#220047")
+    nt1=Button(root,text="New Game",font='roboto 25',bg="#CE9141", fg="#220047")
     nt1.bind("<Button-1>",newgame1)
-    nt1.grid(row=5,column=4,columnspan=2)
-
-
+    nt1.grid(row=5,column=3,columnspan=2)
+    
+    nt2=Button(root,text="Start Game",font='roboto 25',bg="#CE9141", fg="#220047")
+    nt2.bind("<Button-1>",enablebuttton)
+    nt2.grid(row=4,rowspan=2,column=3,columnspan=2)
 
     buttons = StringVar()
 
-    label = Label(root, text="Player Name1:", font='roboto 25',fg="#CE9141", bg="#220047", height=1)
-    label.grid(row=3, column=4)
+    label = Label(root, text="Player Name1:", font='roboto 20',fg="#CE9141", bg="#220047", height=1)
+    label.grid(row=3, column=3)
 
 
-    label = Label(root, text="Player Name2:", font='roboto 25', fg="#CE9141", bg="#220047", height=1)
-    label.grid(row=4, column=4)
+    label = Label(root, text="Player Name2:", font='roboto 20', fg="#CE9141", bg="#220047", height=1)
+    label.grid(row=4, column=3)
 
     button1 = Button(root, text=" ", font='roboto 25', bg="#CE9141", fg="#220047", height=4, width=8, command=lambda: btnClick(button1))
     button1.grid(row=3, column=0)
@@ -122,6 +140,8 @@ def GUI():
 
     button9 = Button(root, text=' ', font='roboto 25', bg="#CE9141", fg="#220047", height=4, width=8, command=lambda: btnClick(button9))
     button9.grid(row=5, column=2)
-
+    
+    disableButton()
+    
     root.mainloop()
 GUI()
